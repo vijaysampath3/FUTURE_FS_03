a0fb2b5e62800c494ebd62454cfc742373ceb9f0
vijay sampath <vijaysampath054@gmail.com>
2026-05-28 10:49:08 +0530

    fix: added tailwindcss vite plugin and updated fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "path";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     TanStackRouterVite({ routesDirectory: "./src/routes" }),
     react(),
   ],
